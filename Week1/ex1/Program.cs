5208c665c4b292eee4a97832d51193480ad49c5f
catalin <catalin@LAPTOP-G11A28AJ>
2020-03-06 16:23:35 +0200

    ex1

---
+++ Week1/ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 3 chars:");
+
+             Stack<char> myStack = new Stack<char>();
+
+            int stackLength = 3;
+
+            for(int i = 0; i < stackLength; i++)
+            {
+                Console.WriteLine("");
+                var item = Console.ReadKey();
+
+                myStack.Push(item.KeyChar);
+               
+            }
+
+            Console.WriteLine("\nThe entered arguments in reverse order are:");
+
+            for (int i = 0; i < stackLength; i++)
+            {
+                //Console.Write(myStack.;
+                Console.WriteLine(myStack.Pop());
+                
+            }
+
+        }
+    }
+
+    
+}
