f92d5dff77ad0f498801eace166ed74a70a091a4
catalin <catalin@LAPTOP-G11A28AJ>
2020-03-26 15:42:11 +0200

    Week4 - ex1 part

---
+++ Week4/LinkQ/Program.cs
@@ -0,0 +1,160 @@
+﻿namespace LinqAndLamdaExpressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<User> allUsers = ReadUsers("users.json");
+            List<Post> allPosts = ReadPosts("posts.json");
+
+            #region Demo
+
+            // 1 - find all users having email ending with ".net".
+            var users1 = from user in allUsers
+                         where user.Email.EndsWith(".net")
+                         select user;
+
+            var users11 = allUsers.Where(user => user.Email.EndsWith(".net"));
+
+            IEnumerable<string> userNames = from user in allUsers
+                                            select user.Name;
+
+            var userNames2 = allUsers.Select(user => user.Name);
+
+            foreach (var value in userNames2)
+            {
+                Console.WriteLine(value);
+            }
+
+            IEnumerable<Company> allCompanies = from user in allUsers
+                                                select user.Company;
+
+            var users2 = from user in allUsers
+                         orderby user.Email
+                         select user;
+
+            var netUser = allUsers.First(user => user.Email.Contains(".net"));
+            Console.WriteLine(netUser.Username);
+
+            #endregion
+
+            // 2 - find all posts for users having email ending with ".net".
+            IEnumerable<int> usersIdsWithDotNetMails = from user in allUsers
+                                                       where user.Email.EndsWith(".net")
+                                                       select user.Id;
+
+            IEnumerable<Post> posts = from post in allPosts
+                                      where usersIdsWithDotNetMails.Contains(post.UserId)
+                                      select post;
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine("PostId = " + post.Id + " " + "user: " + post.UserId);
+            }
+
+
+            // 3 - print number of posts for each user.
+
+            Console.WriteLine("\n############### EX 3  Number of posts for every user :##############");
+
+
+
+            var postGroups = (from post in allPosts
+                              group post by post.UserId into g
+                              select new
+                              {
+                                  c = g.Key,
+                                  count = g.Count(),
+                              }).OrderBy(c => c.count);
+
+            foreach (var x in postGroups)
+            {
+                var userX = allUsers.Single(u => u.Id == x.c);
+                Console.WriteLine("User Name : " + userX.Name + ": " + x.count);
+            }
+
+
+
+
+
+            // 4 - find all users that have lat and long negative.
+            Console.WriteLine("\n############### EX 4 find all users that have lat and long negative :##############");
+            IEnumerable<User> usersWithNegativeCoord = from user in allUsers
+                                                       where user.Address.Geo.Lat < 0
+                                                       where user.Address.Geo.Lng < 0
+                                                       select user;
+            foreach (var user in usersWithNegativeCoord)
+            {
+                Console.WriteLine("User " + user.Name + "Has Negative Coord: [" + user.Address.Geo.Lat + "," + user.Address.Geo.Lng +"]" );
+            }
+
+
+            //############################
+            // 5 - find the post with longest body.
+            // 6 - print the name of the employee that have post with longest body.
+            Console.WriteLine("\n############### EX 5  find the post with longest body. + 6 (find UserName) :##############");
+
+            var longestPost = allPosts
+                                    .OrderByDescending(post => post.Body.Length)
+                                    .Select(post => new { post.Id, post.Body.Length, post.UserId });
+            //get the user details
+            var theUser = allUsers.Single(u => u.Id == longestPost.First().UserId);
+            Console.WriteLine("The longest post is with ID = {0} Length: {1} by {2}", longestPost.First().Id, longestPost.First().Length, theUser.Name);
+
+
+            // 7 - select all addresses in a new List<Address>. print the list.
+            Console.WriteLine("\n############### EX 7 - select all addresses in a new List<Address>. print the list. :##############");
+
+            var addressList =
+                            (from user in allUsers
+                             select new 
+                             {
+                                 user.Name,
+                                 user.Address.City,
+                                 user.Address.Street,
+                                 user.Address.Suite,
+                                 user.Address.Zipcode,
+                                 user.Address.Geo
+
+                             }
+                            ).ToList();
+
+           
+            foreach( var userAddress in addressList)
+            {
+                Console.WriteLine("User {0} Address is: City {1}, Street {2}, Suite {3} , Zip {4}, Coord: {5} - {6}", 
+                    userAddress.Name, userAddress.City, userAddress.Street, userAddress.Suite, userAddress.Zipcode, userAddress.Geo.Lat, userAddress.Geo.Lng
+                    );
+            }
+
+            // 8 - print the user with min lat
+
+
+            // 9 - print the user with max long
+
+
+            // 10 - create a new class: public class UserPosts { public User User {get; set}; public List<Post> Posts {get; set} }
+            //    - create a new list: List<UserPosts>
+            //    - insert in this list each user with his posts only
+
+            // 11 - order users by zip code
+
+            // 12 - order users by number of posts
+        }
+
+        private static List<Post> ReadPosts(string file)
+        {
+            return ReadData.ReadFrom<Post>(file);
+        }
+
+        private static List<User> ReadUsers(string file)
+        {
+            return ReadData.ReadFrom<User>(file);
+        }
+    }
+}
