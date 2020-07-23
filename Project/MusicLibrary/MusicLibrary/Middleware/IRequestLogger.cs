d5da14d4759078c1ec35f79a6693d89b4e42fb99
catalin <catalin@LAPTOP>
2020-07-23 19:32:13 +0300

    add logger

---
+++ Project/MusicLibrary/MusicLibrary/Middleware/IRequestLogger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace MusicLibrary.api.Middleware
+{
+    public interface IRequestLogger
+    {
+        void LogInfo(string message);
+    }
+
+    public class RequestLogger : IRequestLogger
+    {
+        private readonly Guid id;
+        private readonly ILogger<RequestLogger> logger;
+
+        public RequestLogger(ILogger<RequestLogger> logger)
+        {
+            this.id = Guid.NewGuid();
+
+            this.logger = logger;
+        }
+
+        public void LogInfo(string message)
+        {
+            this.logger.LogInformation($"{this.id} : {message}");
+        }
+    }
+}
---
+++ Project/MusicLibraryApplication/MusicLibraryApplication/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MusicLibraryApplication
+{
+    public class Class1
+    {
+    }
+}
