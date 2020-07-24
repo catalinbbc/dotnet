f3b8dadd998f45e4d9f0f8d942aa48852b0daeec
catalin <catalin@LAPTOP>
2020-07-24 15:50:50 +0300

    Apl Client Update

--- Project/WebApiClient/WebApiClient/Startup.cs
+++ Project/WebApiClient/WebApiClient/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using WebApiClient.Helpers;
 
 namespace WebApiClient
 {
@@ -16,6 +19,24 @@ namespace WebApiClient
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddHttpClient();
+
+            services.AddHttpClient("albums", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:554433/");
+                client.Timeout = TimeSpan.FromMinutes(1);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+            
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Music Library Client", Version = "v1.1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +47,17 @@ namespace WebApiClient
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Music Library Client V1.1"); });
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
